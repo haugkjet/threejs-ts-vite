44c0372feb7cfebe176135f1774c18089e7ffe58
Kjetil H <haugkjet@gmail.com>
2023-04-27 13:42:43 +0200

    Wrong path in orbitcontrol import

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import * as THREE from "three";
 
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+
 const scene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(
-  50,
+  75,
   window.innerWidth / window.innerHeight,
   0.1,
   1000
@@ -15,6 +17,8 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+new OrbitControls(camera, renderer.domElement);
 
 const geometry = new THREE.BoxGeometry();
 const material = new THREE.MeshBasicMaterial({
